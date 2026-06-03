a536f55d0d7f6ce0c6617cd474a86eba2d3560d9
blotalt <baska7gzihkara71@gmail.com>
2026-06-03 19:21:25 +0700

    first commit

---
+++ resources/views/index.blade.php
@@ -0,0 +1,10 @@
+@extends('layout')
+@section('content')
+<h1>Homepage</h1>
+
+<body>
+    <h1>Home Page</h1>
+    <iframe width="560" height="315" src="https://www.youtube.com/embed/tRJAn5GnnO8?si=3yFEZ5GwYlsLGPoz" title="YouTube video player" frameborder="0" allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
+
+</body>
+</html>
--- routes/web.php
+++ routes/web.php
@@ -1,7 +1,9 @@
 <?php
 
 use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\UserController;
+use App\Http\Controllers\PostController;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
 });
