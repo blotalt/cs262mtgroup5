6dfd9867738c04f8375817c170873c481e698de3
blotalt <baska7gzihkara71@gmail.com>
2026-06-04 23:53:51 +0700

    new update

--- resources/views/signup.blade.php
+++ resources/views/signup.blade.php
@@ -7,12 +7,21 @@
 </head>
 <body>
     <h1>Sign Up Page</h1>
-    <form action="/register" method="post" >
+    <form action="/register" method="post">
         @csrf
-        <input type="text" name="name" class="mt-2 form-control" placeholder="Username">
+
+        @if ($errors->any())
+            <div style="color:red; border:1px solid red; padding:8px; margin:8px 0;">
+                @foreach ($errors->all() as $error)
+                    <p>{{ $error }}</p>
+                @endforeach
+            </div>
+        @endif
+
+        <input type="text" name="name" class="mt-2 form-control" placeholder="Username" value="{{ old('name') }}">
         <input type="password" name="password" class="mt-2 form-control" placeholder="Password">
         <input type="password" name="password_confirmation" class="mt-2 form-control" placeholder="Repeat Password">
-        <input type="text" name="email" class="my-2 form-control" placeholder="E-mail">
+        <input type="text" name="email" class="my-2 form-control" placeholder="E-mail" value="{{ old('email') }}">
         <br>
         <button type="submit" name="submit" class="btn btn-primary my-2">SIGN UP</button>
     </form>
