113f6bc28a8d143f78f50718002936b1873c7cc6
Chaktra <lhuy02142gmail.com>
2026-06-11 02:47:10 +0700

    Add market price CRUD feature

--- routes/web.php
+++ routes/web.php
@@ -1,53 +1,117 @@
 <?php
 
+use Illuminate\Support\Facades\Route;
 use App\Http\Controllers\UserController;
 use App\Http\Controllers\PostController;
-use Illuminate\Support\Facades\Route;
+use App\Http\Controllers\MarketPriceController;
 
- Route::get('/', function () {
-     return view('home');
- });
+/*
+|--------------------------------------------------------------------------
+| Public Pages
+|--------------------------------------------------------------------------
+*/
 
+Route::get('/', function () {
+    return view('home');
+})->name('home');
 
 Route::get('/home', function () {
     return view('home');
 });
 
-Route::get('/pricemarket', function () {
-    return view('pricemarket');
-});
+Route::get('/pricemarket', [MarketPriceController::class, 'index'])
+    ->name('pricemarket');
 
 Route::get('/variety', function () {
     return view('variety');
-});
+})->name('variety');
+
+Route::get('/about', function () {
+    return view('about');
+})->name('about');
+
+Route::get('/news', [PostController::class, 'news'])->name('news');
+
+/*
+|--------------------------------------------------------------------------
+| Authentication
+|--------------------------------------------------------------------------
+*/
 
 Route::get('/signup', function () {
     return view('signup');
-});
+})->name('register');
 
 Route::get('/login', function () {
     return view('login');
 })->name('login');
 
-Route::get('/dashboard', function () {
-    return view('dashboard');
-});
+Route::post('/register', [UserController::class, 'register'])
+    ->name('register.store');
+
+Route::post('/login', [UserController::class, 'login'])
+    ->name('login.submit');
+
+Route::post('/logout', [UserController::class, 'logout'])
+    ->name('logout');
+
+/*
+|--------------------------------------------------------------------------
+| Dashboard
+|--------------------------------------------------------------------------
+*/
 
 Route::get('/dashboard', function () {
     $posts = auth()->user()->usersCoolPosts()->latest()->get();
-    return view('dashboard', ['posts' => $posts]);
-})->middleware('auth');
 
-Route::get('/news', [PostController::class, 'news']);
-Route::get('/about', function () {
-    return view('about');
+    return view('dashboard', compact('posts'));
+})->middleware('auth')->name('dashboard');
+
+/*
+|--------------------------------------------------------------------------
+| Market Price CRUD (Admin)
+|--------------------------------------------------------------------------
+*/
+
+Route::middleware(['auth'])->prefix('admin')->name('admin.')->group(function () {
+
+    Route::get('/market-prices', [MarketPriceController::class, 'adminIndex'])
+        ->name('market-prices.index');
+
+    Route::get('/market-prices/create', [MarketPriceController::class, 'create'])
+        ->name('market-prices.create');
+
+    Route::post('/market-prices', [MarketPriceController::class, 'store'])
+        ->name('market-prices.store');
+
+    Route::get('/market-prices/{marketPrice}/edit', [MarketPriceController::class, 'edit'])
+        ->name('market-prices.edit');
+
+    Route::put('/market-prices/{marketPrice}', [MarketPriceController::class, 'update'])
+        ->name('market-prices.update');
+
+    Route::delete('/market-prices/{marketPrice}', [MarketPriceController::class, 'destroy'])
+        ->name('market-prices.destroy');
 });
 
-Route::post('/register', [UserController::class, 'register']);
-Route::post('/login', [UserController::class, 'login']);
-Route::post('/logout', [UserController::class, 'logout']);
+/*
+|--------------------------------------------------------------------------
+| Posts
+|--------------------------------------------------------------------------
+*/
 
-Route::post('/create-post', [PostController::class, 'createPost']);
-Route::get('/edit-post/{post}', [PostController::class, 'showEditScreen']);
-Route::put('/edit-post/{post}', [PostController::class, 'updatePost']);
-Route::delete('/delete-post/{post}', [PostController::class, 'deletePost']);
+Route::post('/create-post', [PostController::class, 'createPost'])
+    ->middleware('auth')
+    ->name('posts.store');
+
+Route::get('/edit-post/{post}', [PostController::class, 'showEditScreen'])
+    ->middleware('auth')
+    ->name('posts.edit');
+
+Route::put('/edit-post/{post}', [PostController::class, 'updatePost'])
+    ->middleware('auth')
+    ->name('posts.update');
+
+Route::delete('/delete-post/{post}', [PostController::class, 'deletePost'])
+    ->middleware('auth')
+    ->name('posts.destroy');
